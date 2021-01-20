9c4f7322d320c41d862d20b72d8f224a5550ffc3
Tariqpgr8 <tariqparvez@rocketmail.com>
2021-01-20 17:50:32 -0500

    Initial Commit

--- src/main/java/org/tariq/entity/Department.java
+++ src/main/java/org/tariq/entity/Department.java
@@ -1,4 +1,32 @@
 package org.tariq.entity;
 
-public class Department {
-}
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Department_Table")
+public class Department extends PanacheEntity{
+        @Column(table ="Department_Table",name = "Employee_Name")
+        private long d_id;
+
+        @Column(table ="Department_Table",name = "Employee_Name")
+        private String d_name;
+
+
+        @Column(table="Department_Table", name="Employee_Address")
+        private String location;
+
+        public Department(long d_id, String d_name, String location ) {
+            this.d_id=d_id;
+            this.d_name = d_name;
+            this.location=location;
+        }
+
+        public Department() {
+
+        }
+
+    }
