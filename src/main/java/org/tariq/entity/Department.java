a00d53f87c8556366c4930348dfb9c283e4b9cd8
Tariqpgr8 <tariqparvez@rocketmail.com>
2021-01-20 21:45:55 -0500

    Initial Commit

--- src/main/java/org/tariq/entity/Department.java
+++ src/main/java/org/tariq/entity/Department.java
@@ -15,14 +15,13 @@ public class Department extends PanacheEntity{
         @Column(table ="Department_Table",name = "Employee_Name")
         private String d_name;
 
+        //@Column(table="Department_Table", name="Employee_Address")
+        //private String address;
 
-        @Column(table="Department_Table", name="Employee_Address")
-        private String location;
-
-        public Department(long d_id, String d_name, String location ) {
+        public Department(long d_id, String d_name, String address ) {
             this.d_id=d_id;
             this.d_name = d_name;
-            this.location=location;
+           // this.address=address;
         }
 
         public Department() {
